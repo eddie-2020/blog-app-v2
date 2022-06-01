e75ebf87565a6b44901a4ebaafefacdec53261c0
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-06-01 23:57:01 +0100

    Blog app - controllers specs

---
+++ spec/requests/posts_request_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'Get /index' do
+    before :each do
+      get '/users/1/posts'
+    end
+
+    it 'should respond to the correct http status' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'should render the correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'should respond to the correct placeholder text' do
+      expect(response.body).to include('Here is a list of posts')
+    end
+  end
+
+  describe 'Get /show' do
+    before :each do
+      get '/users/1/posts/1'
+    end
+
+    it 'should respond to the correct http status' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'should render the correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should respond to the correct placeholder text' do
+      expect(response.body).to include('Here is a list of posts for a given user')
+    end
+  end
+end
---
+++ spec/requests/users_request_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'Get /index' do
+    before :each do
+      get '/users'
+    end
+
+    it 'should respond to the correct http status' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'should render the correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'should respond to the correct placeholder text' do
+      expect(response.body).to include('Here is the users home page')
+    end
+  end
+
+  describe 'Get /show' do
+    before :each do
+      get '/users/1'
+    end
+
+    it 'should respond to the correct http status' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'should render the correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should respond to the correct placeholder text' do
+      expect(response.body).to include('Here is the users show page')
+    end
+  end
+end
