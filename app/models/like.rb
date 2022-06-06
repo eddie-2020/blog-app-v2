3a0b11e3f9dd57dc4a367108a6a1865642efdb99
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-06-06 19:35:46 +0100

    Blog app - processing data in models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :users
   belongs_to :posts
+
+  after_save :update_likes_counter
+
+  def update_likes_counter
+    posts.increment!(:likes_counter)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :posts, class_name: 'Post', foreign_key: 'user_id'
+  has_many :commemts, class_name: 'Comment', foreign_key: 'user_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'user_id'
+
+  after_save :three_most_recent_posts
+
+  def three_most_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
