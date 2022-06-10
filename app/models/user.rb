b0830fccec61852128c2a3cf00c8ad5aa241b24b
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-06-10 22:04:49 +0100

    Blog app - generate form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'users_id'
-  has_many :comments, foreign_key: 'users_id'
-  has_many :likes, foreign_key: 'users_id'
+  has_many :comments, foreign_key: 'posts_id'
+  has_many :likes, foreign_key: 'posts_id'
 
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
