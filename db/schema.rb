f97818d7823375d9d5f621a8977ec3acebd17ce1
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-06-14 12:12:15 +0100

    Blog app - fix required changes

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_13_144500) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "surname"
